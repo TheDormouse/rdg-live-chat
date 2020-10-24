1d0db48eeef9ef4e455252520a05e451613f24f4
Raeeddoesgaming <69373544+Raeeddoesgaming@users.noreply.github.com>
2020-10-23 22:14:25 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
@@ -6,7 +5,6 @@ const INDEX = '/index.html';
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-const io = io(server);
 
 const users = {}
 
