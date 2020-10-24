3825c91e098642b7fe6e5d0d0dec36c51a117ea3
Raeeddoesgaming <69373544+Raeeddoesgaming@users.noreply.github.com>
2020-10-24 09:57:43 -0400

    HELP ME

--- server.js
+++ server.js
@@ -23,7 +23,12 @@ const users = {}
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
+    if(name == "null"){
+        socket.broadcast.emit('null-user', name)
+        delete users[socket.id]
+    }else{
     socket.broadcast.emit('user-connected', name)
+    }
   })
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
