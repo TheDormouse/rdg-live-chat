8872070a6801cbf3b5e29d025b275362b3715009
Raeeddoesgaming <69373544+Raeeddoesgaming@users.noreply.github.com>
2020-10-23 23:23:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -21,10 +21,6 @@ const users = {}
 io.on('connection', socket =>{
     socket.on('new-user', name =>{
         users[socket.id] = name       
-         if(name == "null"){
-            socket.broadcast.emit('null-user', name)
-            delete users[socket.id]
-        }else{
         socket.broadcast.emit('user-connected', name)
         }
     })
