ca0a17f37a057d23175b4d60e27cef32d01ca130
Raeeddoesgaming <69373544+Raeeddoesgaming@users.noreply.github.com>
2020-10-24 22:56:06 -0400

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:3000')
+const socket = io.connect('http://live-chat-raeed.herokuapp.com:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
